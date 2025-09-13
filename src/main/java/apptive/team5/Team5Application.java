f054e05cb825257a6e346a0930f8bdcd0f38b0c6
jinwon1234 <ljw0626@naver.com>
2025-09-13 22:47:43 +0900

    feat: 음악 일기 목데이터 반환

--- src/main/java/apptive/team5/Team5Application.java
+++ src/main/java/apptive/team5/Team5Application.java
@@ -3,11 +3,15 @@ package apptive.team5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class Team5Application {
 
 	public static void main(String[] args) {
