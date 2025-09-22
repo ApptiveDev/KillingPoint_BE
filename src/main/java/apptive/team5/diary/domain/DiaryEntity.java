e36ebf6865f22cb497bb8e0fe5512e8bab49d722
sinsehwan <sk124590@gmail.com>
2025-09-22 14:19:09 +0900

    feat(diary): implement diary domain #5

---
+++ src/main/java/apptive/team5/diary/domain/DiaryEntity.java
@@ -0,0 +1,47 @@
+package apptive.team5.diary.domain;
+
+import apptive.team5.user.domain.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DiaryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "diary_id")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false, length = 255)
+    private String artist;
+    @Column(nullable = false, length = 255)
+    private String musicTitle;
+
+    @Column(columnDefinition = "TEXT")
+    private String videoUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_diary_user_id_ref_user_id")
+    )
+    private UserEntity user;
+}
