580a69795da25294c53f48adeead0634e44b851f
sinsehwan <sk124590@gmail.com>
2025-09-23 01:47:03 +0900

    feat(diary-test): implement diaryServiceTest #5

--- src/main/java/apptive/team5/diary/service/DiaryService.java
+++ src/main/java/apptive/team5/diary/service/DiaryService.java
@@ -5,7 +5,6 @@ import apptive.team5.diary.dto.DiaryDeleteRequest;
 import apptive.team5.diary.dto.DiaryRequest;
 import apptive.team5.diary.dto.DiaryResponse;
 import apptive.team5.diary.dto.DiaryUpdateRequest;
-import apptive.team5.diary.repository.DiaryRepository;
 import apptive.team5.user.domain.UserEntity;
 import apptive.team5.user.service.UserLowService;
 import apptive.team5.youtube.dto.YoutubeSearchRequest;
@@ -13,7 +12,6 @@ import apptive.team5.youtube.dto.YoutubeVideoResponse;
 import apptive.team5.youtube.service.YoutubeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,45 +25,6 @@ public class DiaryService {
     private final UserLowService userLowService;
     private final DiaryLowService diaryLowService;
     private final YoutubeService youtubeService;
-
-    @Transactional(readOnly = true)
-    public Page<DiaryResponse> getMyDiaries2(String identifier, Pageable pageable) {
-
-        UserEntity findUser = findUserByIdentifier(identifier);
-
-        /**
-         * 조회된 유저를 바탕으로 음악 일기 찾기
-         */
-
-
-        DiaryResponse data1 =
-                new DiaryResponse("artistName", "musicTitle", "https://www.sleek-mag.com/wp-content/uploads/2016/08/AlbumCovers_Blonde-1200x1200.jpg",
-                        "목데이터1", "https://www.youtube-nocookie.com/embed/UXSdbtfw9Rc");
-
-        DiaryResponse data2 =
-                new DiaryResponse(
-                        "artistName", "musicTitle", "https://www.sleek-mag.com/wp-content/uploads/2016/08/AlbumCovers_Blonde-1200x1200.jpg",
-                        "목데이터2", "https://www.youtube-nocookie.com/embed/UXSdbtfw9Rc");
-
-        DiaryResponse data3 =
-                new DiaryResponse(
-                        "artistName", "musicTitle", "https://www.sleek-mag.com/wp-content/uploads/2016/08/AlbumCovers_Blonde-1200x1200.jpg",
-                        "목데이터1", "https://www.youtube-nocookie.com/embed/UXSdbtfw9Rc");
-
-        DiaryResponse data4 =
-                new DiaryResponse(
-                        "artistName", "musicTitle", "https://www.sleek-mag.com/wp-content/uploads/2016/08/AlbumCovers_Blonde-1200x1200.jpg",
-                        "목데이터2", "https://www.youtube-nocookie.com/embed/UXSdbtfw9Rc");
-
-        List<DiaryResponse> datas = List.of(data1, data2, data3, data4);
-
-        int start = (int) pageable.getOffset();
-        int end = Math.min(start + pageable.getPageSize(), datas.size());
-        List<DiaryResponse> subList = datas.subList(start, end);
-
-        return new PageImpl<>(subList, pageable, datas.size());
-
-    }
 
     @Transactional(readOnly = true)
     public Page<DiaryResponse> getMyDiaries(String identifier, Pageable pageable) {
---
+++ src/test/java/apptive/team5/diary/service/DiaryServiceTest.java
@@ -0,0 +1,109 @@
+package apptive.team5.diary.service;
+
+import apptive.team5.diary.domain.DiaryEntity;
+import apptive.team5.diary.dto.DiaryRequest;
+import apptive.team5.diary.dto.DiaryResponse;
+import apptive.team5.diary.dto.DiaryUpdateRequest;
+import apptive.team5.user.domain.UserEntity;
+import apptive.team5.user.service.UserLowService;
+import apptive.team5.util.TestUtil;
+import apptive.team5.youtube.dto.YoutubeVideoResponse;
+import apptive.team5.youtube.service.YoutubeService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class DiaryServiceTest {
+    @InjectMocks
+    private DiaryService diaryService;
+
+    @Mock
+    private UserLowService userLowService;
+
+    @Mock
+    private DiaryLowService diaryLowService;
+
+    @Mock
+    private YoutubeService youtubeService;
+
+    @Test
+    @DisplayName("내 다이어리 목록 조회")
+    void getMyDiaries() {
+        // given
+        UserEntity user = TestUtil.makeUserEntity();
+        DiaryEntity diary = new DiaryEntity("hihi music", "chopin", "image.url", "video.url", "test content", user);
+        Page<DiaryEntity> diaryEntityPage = new PageImpl<>(List.of(diary));
+        PageRequest pageRequest = PageRequest.of(0, 5);
+
+        given(userLowService.findByIdentifier(user.getIdentifier())).willReturn(user);
+        given(diaryLowService.findDiaryByUser(user, pageRequest)).willReturn(diaryEntityPage);
+
+        // when
+        Page<DiaryResponse> result = diaryService.getMyDiaries(user.getIdentifier(), pageRequest);
+
+        // then
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent().get(0).musicTitle()).isEqualTo("hihi music");
+        verify(userLowService).findByIdentifier(any(String.class));
+        verify(diaryLowService).findDiaryByUser(any(UserEntity.class), any(PageRequest.class));
+    }
+
+    @Test
+    @DisplayName("다이어리 생성")
+    void createDiary() {
+        // given
+        UserEntity user = TestUtil.makeUserEntity();
+        DiaryRequest diaryRequest = new DiaryRequest("rach", "concerto", "image.url", "test content");
+        YoutubeVideoResponse youtubeVideoResponse = new YoutubeVideoResponse("Test Title", "PT3M5S", "video.url");
+
+        given(userLowService.findByIdentifier(user.getIdentifier())).willReturn(user);
+        given(youtubeService.searchVideo(any())).willReturn(Collections.singletonList(youtubeVideoResponse));
+
+        // when
+        diaryService.createDiary(user.getIdentifier(), diaryRequest);
+
+        // then
+        verify(userLowService).findByIdentifier(any(String.class));
+        verify(youtubeService).searchVideo(any());
+        verify(diaryLowService).saveDiary(any(DiaryEntity.class));
+    }
+
+    @Test
+    @DisplayName("다이어리 수정")
+    void updateDiary() {
+        // given
+        UserEntity user = TestUtil.makeUserEntity();
+        Long diaryId = 1L;
+        DiaryUpdateRequest updateRequest = new DiaryUpdateRequest("Updated Artist", "concerto 2", "updated image url", "updated content");
+        DiaryEntity diary = new DiaryEntity("Test Music", "Test Artist", "image.url", "video.url", "Test content", user);
+        YoutubeVideoResponse youtubeVideoResponse = new YoutubeVideoResponse("Updated Title", "PT3M5S", "updated.video.url");
+
+        given(userLowService.findByIdentifier(user.getIdentifier())).willReturn(user);
+        given(diaryLowService.findDiaryById(diaryId)).willReturn(diary);
+        given(youtubeService.searchVideo(any())).willReturn(Collections.singletonList(youtubeVideoResponse));
+
+        // when
+        diaryService.updateDiary(user.getIdentifier(), diaryId, updateRequest);
+
+        // then
+        verify(userLowService).findByIdentifier(any(String.class));
+        verify(diaryLowService).findDiaryById(any(Long.class));
+        verify(youtubeService).searchVideo(any());
+        verify(diaryLowService).updateDiary(any(UserEntity.class), any(DiaryEntity.class), any());
+    }
+}
