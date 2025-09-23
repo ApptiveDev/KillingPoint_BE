a1857efb1739e79f61018e143f2d9f2a06dc3e82
sinsehwan <sk124590@gmail.com>
2025-09-23 15:08:09 +0900

    chore(diary): chore diaryRequest (notblank, message) #5

--- src/main/java/apptive/team5/diary/dto/DiaryRequest.java
+++ src/main/java/apptive/team5/diary/dto/DiaryRequest.java
@@ -2,16 +2,17 @@ package apptive.team5.diary.dto;
 
 import apptive.team5.diary.domain.DiaryEntity;
 import apptive.team5.user.domain.UserEntity;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
 public record DiaryRequest(
-        @NotEmpty
+        @NotBlank(message = "아티스트는 필수 입력입니다.")
         String artist,
-        @NotEmpty
+        @NotBlank(message = "음악 제목은 필수 입력입니다.")
         String musicTitle,
-        @NotEmpty
+        @NotBlank(message = "앨범 이미지 URL은 비워둘 수 없습니다.")
         String albumImageUrl,
-        @NotEmpty
+        @NotBlank(message = "내용을 입력해주세요")
         String content
 ) {
         public static DiaryEntity toEntity(DiaryRequest diaryRequest, String videoUrl, UserEntity user) {
