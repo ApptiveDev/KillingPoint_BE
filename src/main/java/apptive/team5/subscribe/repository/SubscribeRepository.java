a57537a6213bf083e6e39f4dd35f29308bfe85e1
jinwon1234 <ljw0626@naver.com>
2025-11-11 21:04:40 +0900

    feat: 구독 추가 테스트 구현

--- src/main/java/apptive/team5/subscribe/repository/SubscribeRepository.java
+++ src/main/java/apptive/team5/subscribe/repository/SubscribeRepository.java
@@ -2,6 +2,12 @@ package apptive.team5.subscribe.repository;
 
 import apptive.team5.subscribe.domain.Subscribe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+
+    @Query("select s from Subscribe s where s.subscriber.id = :subscriberId")
+    List<Subscribe> findBySubscriberId(Long subscriberId);
 }
---
+++ src/test/java/apptive/team5/subscribe/controller/SubscribeControllerTest.java
@@ -0,0 +1,82 @@
+package apptive.team5.subscribe.controller;
+
+
+import apptive.team5.subscribe.domain.Subscribe;
+import apptive.team5.subscribe.repository.SubscribeRepository;
+import apptive.team5.user.domain.UserEntity;
+import apptive.team5.user.repository.UserRepository;
+import apptive.team5.util.TestSecurityContextHolderInjection;
+import apptive.team5.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.SoftAssertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.securityContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class SubscribeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SubscribeRepository subscribeRepository;
+
+
+    @DisplayName("구독 추가 성공")
+    @Test
+    void subscribeSuccess() throws Exception {
+
+        // given
+        UserEntity subscriber = TestUtil.makeUserEntity();
+        userRepository.save(subscriber);
+        UserEntity subscribedTo = TestUtil.makeDifferentUserEntity(subscriber);
+        userRepository.save(subscribedTo);
+
+        TestSecurityContextHolderInjection.inject(subscriber.getId(), subscriber.getRoleType());
+
+        mockMvc.perform(post("/api/subscribes/{subscribeToId}", subscribedTo.getId())
+                .with(securityContext(SecurityContextHolder.getContext()))
+        ).andExpect(status().isCreated());
+
+        List<Subscribe> subscribes = subscribeRepository.findBySubscriberId(subscriber.getId());
+
+        assertSoftly(softly -> {
+            softly.assertThat(subscribes).hasSize(1);
+            softly.assertThat(subscribes.getFirst().getSubscribedTo().getId()).isEqualTo(subscribedTo.getId());
+        });
+    }
+
+    @DisplayName("구독 추가 실패 - 존재하지 않는 회원")
+    @Test
+    void subscribeFail() throws Exception {
+
+        // given
+        UserEntity subscriber = TestUtil.makeUserEntity();
+        userRepository.save(subscriber);
+
+        TestSecurityContextHolderInjection.inject(subscriber.getId(), subscriber.getRoleType());
+
+        // when
+        mockMvc.perform(post("/api/subscribes/{subscribeToId}", 100L)
+                .with(securityContext(SecurityContextHolder.getContext()))
+        ).andExpect(status().isNotFound());
+
+    }
+
+}
