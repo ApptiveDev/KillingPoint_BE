360f01f1a10915f92a008196a100294aa61a75b7
jinwon1234 <ljw0626@naver.com>
2025-11-12 13:47:44 +0900

    chore: FileUploadRequest 에러 메시지 추가

--- src/main/java/apptive/team5/file/dto/FileUploadRequest.java
+++ src/main/java/apptive/team5/file/dto/FileUploadRequest.java
@@ -1,15 +1,14 @@
 package apptive.team5.file.dto;
 
-import apptive.team5.file.domain.TemporalFile;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.URL;
 
 public record FileUploadRequest(
-        @NotNull
+        @NotNull(message = "TemporalFile의 PK를 입력해주세요")
         Long id,
         @URL(message = "url형식에 맞지 않습니다.")
-        @NotBlank
+        @NotBlank(message = "presignedUrl은 필수 값입니다.")
         String presignedUrl
 ) {
 
