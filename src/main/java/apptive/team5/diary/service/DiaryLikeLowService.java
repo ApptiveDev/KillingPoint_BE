c6116c57841801ca14e3e51560ba87083f1c993f
sinsehwan <sk124590@gmail.com>
2025-11-13 09:22:54 +0900

    feat(diary): implement diaryLowService #11

---
+++ src/main/java/apptive/team5/diary/service/DiaryLikeLowService.java
@@ -0,0 +1,36 @@
+package apptive.team5.diary.service;
+
+import apptive.team5.diary.domain.DiaryEntity;
+import apptive.team5.diary.domain.DiaryLikeEntity;
+import apptive.team5.diary.repository.DiaryLikeRepository;
+import apptive.team5.global.exception.NotFoundEntityException;
+import apptive.team5.user.domain.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class DiaryLikeLowService {
+    private final DiaryLikeRepository diaryLikeRepository;
+
+    public DiaryLikeEntity saveDiaryLike(DiaryLikeEntity diaryLike) {
+        return diaryLikeRepository.save(diaryLike);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean existsByUserAndDiary(UserEntity user, DiaryEntity diary) {
+        return diaryLikeRepository.existsByUserAndDiary(user, diary);
+    }
+
+    @Transactional(readOnly = true)
+    public DiaryLikeEntity findByUserAndDiary(UserEntity user, DiaryEntity diary) {
+        return diaryLikeRepository.findByUserAndDiary(user, diary)
+                .orElseThrow(() -> new NotFoundEntityException("좋아요를 누르지 않은 킬링파트입니다!"));
+    }
+
+    public void deleteDiaryLike(DiaryLikeEntity diaryLike) {
+        diaryLikeRepository.delete(diaryLike);
+    }
+}
--- src/main/java/apptive/team5/diary/service/DiaryLikeService.java
+++ src/main/java/apptive/team5/diary/service/DiaryLikeService.java
@@ -2,9 +2,7 @@ package apptive.team5.diary.service;
 
 import apptive.team5.diary.domain.DiaryEntity;
 import apptive.team5.diary.domain.DiaryLikeEntity;
-import apptive.team5.diary.repository.DiaryLikeRepository;
 import apptive.team5.global.exception.DuplicateException;
-import apptive.team5.global.exception.NotFoundEntityException;
 import apptive.team5.user.domain.UserEntity;
 import apptive.team5.user.service.UserLowService;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class DiaryLikeService {
-    private final DiaryLikeRepository diaryLikeRepository;
+    private final DiaryLikeLowService diaryLikeLowService;
     private final UserLowService userLowService;
     private final DiaryLowService diaryLowService;
 
@@ -23,21 +21,20 @@ public class DiaryLikeService {
         UserEntity user = userLowService.getReferenceById(userId);
         DiaryEntity diary = diaryLowService.getReferenceById(diaryId);
 
-        if (diaryLikeRepository.existsByUserAndDiary(user, diary)) {
+        if (diaryLikeLowService.existsByUserAndDiary(user, diary)) {
             throw new DuplicateException("이미 좋아요를 누르셨습니다!");
         }
 
-        diaryLikeRepository.save(new DiaryLikeEntity(user, diary));
+        diaryLikeLowService.saveDiaryLike(new DiaryLikeEntity(user, diary));
     }
 
     public void unlikeDiary(Long userId, Long diaryId) {
         UserEntity user = userLowService.getReferenceById(userId);
         DiaryEntity diary = diaryLowService.getReferenceById(diaryId);
 
-        DiaryLikeEntity diaryLike = diaryLikeRepository.findByUserAndDiary(user, diary)
-                .orElseThrow(() -> new NotFoundEntityException("좋아요를 누르지 않은 킬링파트입니다!"));
+        DiaryLikeEntity diaryLike = diaryLikeLowService.findByUserAndDiary(user, diary);
 
-        diaryLikeRepository.delete(diaryLike);
+        diaryLikeLowService.deleteDiaryLike(diaryLike);
     }
 
 }
