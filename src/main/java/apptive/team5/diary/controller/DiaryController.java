9f10dd185cf7d87db08e7fb396816b29ba828c3c
sinsehwan <sk124590@gmail.com>
2025-11-25 21:23:03 +0900

    feat(diary): apply patchmapping in update #25

--- src/main/java/apptive/team5/diary/controller/DiaryController.java
+++ src/main/java/apptive/team5/diary/controller/DiaryController.java
@@ -13,9 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -97,7 +97,7 @@ public class DiaryController {
         return ResponseEntity.status(HttpStatus.CREATED).location(URI.create("/api/diaries/" + diary.getId())).build();
     }
 
-    @PutMapping("/{diaryId}")
+    @PatchMapping("/{diaryId}")
     public ResponseEntity<Void> updateDiary(
             @AuthenticationPrincipal
             Long userId,
