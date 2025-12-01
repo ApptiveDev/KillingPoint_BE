8f5f6796584ca72446b0ab3174846fd080329c08
jinwon1234 <ljw0626@naver.com>
2025-12-01 14:41:59 +0900

    feat: SurveyEntity 구현

---
+++ src/main/java/apptive/team5/survey/domain/SurveyEntity.java
@@ -0,0 +1,28 @@
+package apptive.team5.survey.domain;
+
+import apptive.team5.user.domain.UserEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SurveyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private UserEntity user;
+
+    public SurveyEntity(String content, UserEntity user) {
+        this.content = content;
+        this.user = user;
+    }
+}
